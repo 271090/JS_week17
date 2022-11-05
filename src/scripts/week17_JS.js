6025c424dd5b30ee4e821f35dbe68b72b3bd4d88
Ольга Малышева <olga.maysheva.smm@gmail.com>
2022-11-05 16:33:02 +0200

    задание 1

--- src/scripts/week17_JS.js
+++ src/scripts/week17_JS.js
@@ -5,12 +5,16 @@ function check() {
   const secondName = document.querySelector('.second_name');
   const thirdName = document.querySelector('.third_name');
   const Name = document.getElementById('name').value;
+  let low = Name.toLowerCase();
 
-  let arrayNew = Name.split(" ");
+  let arrayNew = low.split(" ");
   let first = arrayNew[1];
   let second = arrayNew[0];
   let third = arrayNew[2];
 
+  first = first[0].toUpperCase() + first.slice(1, );
+  second = second[0].toUpperCase() + second.slice(1, );
+  third = third[0].toUpperCase() + third.slice(1, );
   
 
   firstName.innerHTML = `Имя: ${first}`;
